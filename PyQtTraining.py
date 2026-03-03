ce41c1883e1a4991cc6e85e0441a699fda5789f5
KK <kieren.karanjia@gmail.com>
2026-03-03 17:40:03 +1100

    added buttons and text to layout

--- PyQtTraining.py
+++ PyQtTraining.py
@@ -7,7 +7,43 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLay
 #App Settings
 
 app = QApplication([])
-main_window = QWidget
-main_window.setWindowTitle('First Test App')
+main_window = QWidget()
+main_window.setWindowTitle("First Test App")
 main_window.resize(300, 200)
 
+
+#Create Objects/Widgets
+
+title_text = QLabel("Pick your favourite colour")
+title_1 = QLabel("Blue")
+title_2 = QLabel("Red")
+title_3 = QLabel("Purple")
+
+button1 = QPushButton("This one!")
+button2 = QPushButton("Its Red!")
+button3 = QPushButton("Yes it has to be this!")
+
+
+#Structure Layout
+master_layout = QVBoxLayout()
+row1 = QHBoxLayout()
+row2 = QHBoxLayout()
+row3 = QHBoxLayout()
+
+#Import created objects into the layout
+
+row1.addWidget(title_text, alignment = Qt.AlignCenter)
+
+
+row2.addWidget(title_1, alignment = Qt.AlignCenter)
+row2.addWidget(title_2, alignment = Qt.AlignCenter)
+row2.addWidget(title_3, alignment = Qt.AlignCenter)
+
+row3.addWidget(button1, alignment = Qt.AlignCenter)
+row3.addWidget(button2, alignment = Qt.AlignCenter)
+row3.addWidget(button3, alignment = Qt.AlignCenter)
+
+
+#Run the App
+main_window.show()
+app.exec_()
