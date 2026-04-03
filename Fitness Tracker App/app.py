c7fd19ee4e6908826112b97ed2f329e0b86f8511
KK <kieren.karanjia@gmail.com>
2026-04-03 18:16:06 +1100

    added initial UI elements and widgets from PyQt5

--- Fitness Tracker App/app.py
+++ Fitness Tracker App/app.py
@@ -1,6 +1,6 @@
 #imports
 from PyQt5.QtCore import Qt, QDate
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QTableWidget, QTableWidgetItem, QHeaderView, QCheckBox
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QTableWidget, QTableWidgetItem, QHeaderView, QCheckBox, QDateEdit, QLineEdit
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 
 import matplotlib.pyplot as plt
@@ -12,8 +12,38 @@ from sys import exit
 
 
 # Main Class
-Class
+class FitTrack (QWidget):
+    
+    def __init__(self):
+        super().__init__()
     #initial UI
+    def initUI(self):
+        #All left column widgets
+        self.date_box = QDateEdit()
+        self.date_box.setDate(QDate.currentDate())
+
+        self.cal_box = QLineEdit()
+        self.cal_box.setPlaceholderText("Number of Burned Calories")
+
+        self.distance_box = QLineEdit()
+        self.distance_box.setPlaceholderText("Enter Distance Ran")
+
+        self.description = QLineEdit()
+        self.description.setPlaceholderText("Type of Run")
+
+        self.submit_button = QPushButton("Submit")
+        self.add_button = QPushButton("Add")
+        self.delete_button = QPushButton("Delete")
+        self.clear_button = QPushButton("clear")
+        self.dark_box = QCheckBox("Dark Mode")
+
+
+        #All right column widgets
+        self.table = QTableWidget()
+
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+
 
     #load tables
 
