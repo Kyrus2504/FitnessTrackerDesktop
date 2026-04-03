91b3d13da99cc61e53f60df0f5e9ec9f846680ce
KK <kieren.karanjia@gmail.com>
2026-04-03 18:01:35 +1100

    started fitness tracker app and imported imports

---
+++ Fitness Tracker App/app.py
@@ -0,0 +1,33 @@
+#imports
+from PyQt5.QtCore import Qt, QDate
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QTableWidget, QTableWidgetItem, QHeaderView, QCheckBox
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+import numpy as np
+from sys import exit
+
+
+
+# Main Class
+Class
+    #initial UI
+
+    #load tables
+
+    #add tables
+
+    #delete table
+
+    #Calculate calories
+
+    #click
+
+    #dark mode
+
+    #reset
+
+
+#Init DB
